27146c306f7baa58407ae52ff801dce665ac9099
Xushnudd <boshhacker1@gmail.com>
2026-01-04 17:45:10 +0500

    add

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 
-app = Flask(__name__)
+engine = create_engine('sqlite:///maktab.db', echo=True)
 
-@app.route("/")
-def home():
-    return "Salom"
+class Base(DeclarativeBase):
+    pass
 
-if __name__ == "__main__":
-    app.run()
+class User(Base):
+    __tablename__ = "users"
+    
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str]
+    email:Mapped[str]
+    
+Base.metadata.create_all(engine)
+
+with Session(engine) as session:
+    new_user = User(name="Ali", email="ali@example.com")
+    session.add(new_user)
+    session.commit()
+    
+with Session(engine) as session:
+    statement = select(User).where(User.name == "Ali")
+    user = session.execute(statement).scalars().all()
+    for i in user:
+        print(f"ID: {i.id}, Ismi: {i.name}")
